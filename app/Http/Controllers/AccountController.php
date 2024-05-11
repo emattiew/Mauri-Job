e5a013863e8b782a9a43247d70f2e85ca2aac7ec
unknown <amatewva@gmail.com>
2024-05-11 03:45:34 -0700

    firs commit

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    public function registration(){
+        return view('front.account.registration');
+
+    }
+    public function login(){
+
+    }
+    public function precessregistration(){
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AccountController;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/', function () {
+ //   return view('welcome');
+//});
+Route::get('/',[HomeController::class,'index'])->name('home');
+Route::get('/account/register',[AccountController::class,'registration'])->name('account.registration');
